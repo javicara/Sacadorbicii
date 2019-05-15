a892be0781f8602d008938617a4cea4154448b65
javicara <37423364+javicara@users.noreply.github.com>
2019-05-15 06:41:01 -0300

    Version  estable que genera la carpeta en Documentos y saca 	todas las imagenes de tuva. Ademas saca modelo anio, talle, 	precios. Faltaria correguir bien lo del talle

--- index.js
+++ index.js
@@ -3,39 +3,9 @@ const fs = require('fs');
 const request = require('request');
 
 let images = [];
-/*function sacatitulo(){
-    request('https://tuvalum.com/comprar/bicicleta-wilier-35917',(err,res,body)=>{
-        if(!err && res.statusCode==200){
-            let $ = cheerio.load(body);
-            let tituloCompleto = $('h1','.container-fluid');// ya lo tenemos con la propiedad text()
-            //let a=cheerio.load(tituloCompleto);
-             let marca = $('strong','h1.card-name');
-             let anio=$('span','h1.card-name');
-            let precioAntes = $('div.old-price','.flex-row');
-            let precioOferta=$('h3','.flex-row');
-            let sacaTalle=$('div.product-attribute','div.col-sm-12');
-           var talle=talle.text().replace('vendedor','duenio');//
-            
 
-           console.log(tituloCompleto.text()+'hola');
-            
-            console.log( tituloCompleto.text() +' Talle: '+talle+ ' PRECIO: $'+ precioOferta.text() + ' ANTES: $' +precioAntes.text() );
-           console.log(marca.text() +anio.text()+talle+ 'hola');
-            }
-            else{
-                // console.log( res.statusCode );
-                 console.log(res);
-                 console.log(err);
 
-     
-     
-             }
-            });
-        }
-    
-sacatitulo();*/
-
-request('https://tuvalum.com/comprar/bicicleta-wilier-35917', (err, res, body) => {
+request('https://tuvalum.com/comprar/bicicleta-pinarello-25175', (err, res, body) => {
     if (!err && res.statusCode == 200) {
         let $ = cheerio.load(body);
         let tituloCompleto = $('h1', '#name-container');
@@ -53,12 +23,12 @@ request('https://tuvalum.com/comprar/bicicleta-wilier-35917', (err, res, body) =
             //console.log(urlImg);
             console.log(spliteado);//DEVUEVLE TODOS LOS LINKS DE LAS IMAGENES DE TUVALUM
             images.push(spliteado);// arreglo con los links de las imagenes 
-            rutaCarpeta= './'+tituloCompleto.text();
+            rutaCarpeta= '../../BICIS DRIVE/'+tituloCompleto.text();// VA A DOCUMENTOS / BICIS DRIVE 
             var dir = rutaCarpeta// crea el directorio y le guarda las imagenes
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir);
             }
-            for (let i = 1; i < images.length; i++) {
+            for (let i = 0; i < images.length-1; i++) {
                 if (images[i]) {
                     request(images[i]).pipe(fs.createWriteStream(dir+`/photo_${i}.jpg`))
                 }
@@ -77,27 +47,6 @@ request('https://tuvalum.com/comprar/bicicleta-wilier-35917', (err, res, body) =
 
     }
 })
-/*/
 
 
-/*  request('https://miami.craigslist.org/mdc/bik/d/miami-bmc-tm02-time-machine-tt-bike/6882047746.html',(err,res,body)=>{
-        if(!err && res.statusCode==200){
-            let $ = cheerio.load(body);
-            $('img','div').each(function(){
-                var urlImg= $(this).attr('src');
-                console.log(urlImg);
-            });
-
-
-
-        }
-        else{
-           // console.log( res.statusCode );
-            console.log(res);
-            console.log(err);
-
-
-
-        }
-})/*/
 
