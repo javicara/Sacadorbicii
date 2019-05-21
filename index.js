731edd25ced811989ee54ba6e079bda137cfbb17
javier <javiercaraballo97@gmail.com>
2019-05-21 16:09:40 -0300

    no retorna pero andaba, lo hice para volver controlz

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const request = require('request');
 let images = [];
 
 
-request('https://tuvalum.com/comprar/bicicleta-pinarello-25175', (err, res, body) => {
+/*request('https://tuvalum.com/comprar/bicicleta-pinarello-25175', (err, res, body) => {
     if (!err && res.statusCode == 200) {
         let $ = cheerio.load(body);
         let tituloCompleto = $('h1', '#name-container');
@@ -46,7 +46,42 @@ request('https://tuvalum.com/comprar/bicicleta-pinarello-25175', (err, res, body
 
 
     }
-})
+})/*/
+favoritos();
 
+function favoritos (){
+    request('https://tuvalum.com/ofertas?appbundle_filter%5Btext%5D=&appbundle_filter%5Btype%5D=1&appbundle_fi'+
+    'lter%5Bcategory%5D=2&precio_min=1000&precio_max=4000&appbundle_filter%5Bextra_form%5D%5Bbrand%5D=&appbundle_'+
+    'filter%5Bextra_form%5D%5Bmaterial%5D%5B%5D=2&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=22&appbundle_filter'+
+    '%5Bextra_form%5D%5Bsize%5D%5B%5D=30&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=31&appbundle_filter%5Bextra_for'+
+    'm%5D%5Bsize%5D%5B%5D=32&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=33&appbundle_filter%5Bextra_form%5D%5BmainGr'+'oup%5D=&ofertas=1&ofertas=1&appbundle_filter%5B_token%5D=vUTGO2M_fA_zX1g4JfQiizbGaOZMqdqnJD_CZLB_wdg', (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+        let $ = cheerio.load(body);
+        const itemsFavoritos = $('.grid-container .card').toArray()
+        .map(item=>{
+            const $item= $(item);
+                return{
+                    title: $item.find('.card-name').text(),
+                    precioActual:$item.find('.actual-price').text(),
+                    precioViejo:$item.find('.old-price').text(),
+                    link:$item.find('.input-block').first().attr('href'),
+                    talle:$item.find(".card-attributes small:contains('Talla')").children().text(),
+                    //precioVenta:Number($item.find('.actual-price').text())+1500,//queda mal pq me toma el . por ,
+                };
+
+            })            
+             return itemsFavoritos;        
+             debugger;
+        }
+    }).then(itemsFavoritos =>{
+    fs.writeFile('./items.json',JSON.stringify(itemsFavoritos),function(error){
+        if (error) return console.log(error);
+        console.log('items saved! xD en el JSOM');
+
+
+
+    });
+});
+}
 
 
