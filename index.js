f9c6a8283f43585055cb5585bd6f5d25be942de5
javier <javiercaraballo97@gmail.com>
2019-05-22 10:10:03 -0300

    funcion sacalink andando y favoritos tambien

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const baseUrl='https://tuvalum.com';
 
 var linkPrueba=baseUrl+'/comprar/bicicleta-berria-44805';
 
+const cheerio = require('cheerio');
+const fs = require('fs');
+const request = require('request');
+
+let images = [];
+
+
 /*request('https://tuvalum.com/comprar/bicicleta-pinarello-25175', (err, res, body) => {
     if (!err && res.statusCode == 200) {
         let $ = cheerio.load(body);
@@ -18,8 +25,6 @@ var linkPrueba=baseUrl+'/comprar/bicicleta-berria-44805';
         let sacaTalle = $('div.product-attribute', 'div.col-sm-12');
         var talle = sacaTalle.text().replace('vendedor', 'dueño');//
         $('.product-image', 'div.item').each(function () {
-
-
             var urlImg = $(this).attr('style');
             var spliteado = urlImg.split(["'"])[1];
             //console.log(urlImg);
@@ -34,28 +39,22 @@ var linkPrueba=baseUrl+'/comprar/bicicleta-berria-44805';
                 if (images[i]) {
                     request(images[i]).pipe(fs.createWriteStream(dir+`/photo_${i}.jpg`))
                 }
-
             }
         });
         console.log(tituloCompleto.text() + ' Talle: ' + talle + ' PRECIO: $' + precioOferta.text() + ' ANTES: $' + precioAntes.text());
-
     }
     else {
         // console.log( res.statusCode );
         console.log(res);
         console.log(err);
-
-
-
     }
 })/*/
-/*favoritos()
+favoritos();
 
 function favoritos (){
-    url = 'https://tuvalum.com/ofertas?appbundle_filter%5Btext%5D=&appbundle_filter%5Btype%5D=1&appbundle_filter%5Bcategory%5D=2&precio_min=1000&precio_max=4000&appbundle_filter%5Bextra_form%5D%5Bbrand%5D=&appbundle_filter%5Bextra_form%5D%5Bmaterial%5D%5B%5D=2&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=22&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=30&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=31&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=32&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=33&appbundle_filter%5Bextra_form%5D%5BmainGroup%5D=&ofertas=1&ofertas=1&appbundle_filter%5B_token%5D=vUTGO2M_fA_zX1g4JfQiizbGaOZMqdqnJD_CZLB_wdg', (err, res, body) => {
-
-     axios.get(url).then(response =>{
-        let $ = cheerio.load(response);
+    request('https://tuvalum.com/ofertas?appbundle_filter%5Btext%5D=&appbundle_filter%5Btype%5D=1&appbundle_filter%5Bcategory%5D=2&precio_min=1000&precio_max=4000&appbundle_filter%5Bextra_form%5D%5Bbrand%5D=&appbundle_filter%5Bextra_form%5D%5Bmaterial%5D%5B%5D=2&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=22&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=30&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=31&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=32&appbundle_filter%5Bextra_form%5D%5Bsize%5D%5B%5D=33&appbundle_filter%5Bextra_form%5D%5BmainGroup%5D=&ofertas=1&ofertas=1&appbundle_filter%5B_token%5D=vUTGO2M_fA_zX1g4JfQiizbGaOZMqdqnJD_CZLB_wdg', (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+        let $ = cheerio.load(body);
         const itemsFavoritos = $('.grid-container .card').toArray()
         .map(item=>{
             const $item= $(item);
@@ -66,28 +65,20 @@ function favoritos (){
                     link:$item.find('.input-block').first().attr('href'),
                     talle:$item.find(".card-attributes small:contains('Talla')").children().text(),
                     //precioVenta:Number($item.find('.actual-price').text())+1500,//queda mal pq me toma el . por ,
-                    
                 };
-                debugger;
              return itemsFavoritos
-             
-
-        }).then(itemsFavoritos => {
-            console.log('finish!');
-            fs.writeFile('./items.json', JSON.stringify(itemsFavoritos), 'utf8', function(error) {
-                if (error) return console.log('error', error);
-                console.log(itemsFavoritos.length, 'items saved');
-            });
-        
-        
-        });
-    }
-
-        )
+        })
+        debugger;
 
     }
+    else{
+        // console.log( res.statusCode );
+        console.log(res);
+        console.log(err);
+    }
+
 }
-*/
+    )}
 
 
 
